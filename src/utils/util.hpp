3c578a34ce974f05d9f4f3957baede9b3123048c
mbero_akoko <mberoakoko@gmail.com>
2025-06-26 09:57:42 +0200

    implememted map function

--- src/utils/util.hpp
+++ src/utils/util.hpp
@@ -31,7 +31,7 @@ namespace Utils {
     template<typename T, typename E>
     class Result_t {
         std::variant<T, E> data_;
-        bool is_success_ { false };
+        bool is_success_;
 
 
         // Private constructor
@@ -61,6 +61,64 @@ namespace Utils {
             return Result_t(std::in_place_type<E>, std::forward<Args>(args)...);
         }
 
+        // Accessors
+        [[nodiscard]] auto is_ok() const -> bool {return is_success_;}
+        [[nodiscard]] auto is_error() const -> bool {return !is_success_;}
+
+        auto value() const -> T& {
+            if (!is_success_) {
+                throw std::bad_variant_access();
+            }
+            return std::get<T>(data_);
+        }
+
+        auto error() const -> E& {
+            if (is_success_) {
+                throw std::bad_variant_access();
+            }
+            return std::get<E>(data_);
+        }
+        template<typename Dummy_t = void>
+        using DummySFNAE_t = std::enable_if_t<std::is_void_v<T>, Dummy_t>;
+
+        auto value() const -> DummySFNAE_t {
+            if (!is_success_) {
+                throw std::bad_variant_access();
+            }
+            return DummySFNAE_t(std::get<E>(data_));
+        }
+
+        template <typename F, typename U = std::invoke_result_t<F, T>>
+        auto map(F&& f) -> Result_t<U, E> {
+            if (is_ok()) {
+                return Result_t<U, E>::ok(std::invoke(std::forward<F>(f), std::get<T>(std::move(data_))));
+            }
+            return Result_t<U, E>::error(std::get<E>(std::move(data_)));
+        }
+
+        template<typename F, typename U = std::invoke_result_t<F, T>>
+        auto map(F&& f) && -> std::enable_if_t<std::is_void_v<T>, Result_t<U, E>> {
+            if (is_ok()) {
+                return Result_t<U, E>::ok(std::invoke(std::forward<F>(f)));
+            }
+            return Result_t<U, E>::error(std::get<E>(std::move(data_)));
+        }
     };
+
+    template<typename T, typename E>
+    auto make_okay(T value) -> Result_t<E, E> {
+        return Result_t<T, E>::ok(std::move(value));
+    }
+
+    template<typename T, typename E>
+    auto make_error(E error) -> Result_t<T, E> {
+        return Result_t<T, E>::error(std::move(error));
+    }
+
+    template<typename E>
+    auto make_void() -> Result_t<void, E> {
+        return Result_t<void, E>::ok();
+    }
+
 }
 #endif //UTIL_HPP
